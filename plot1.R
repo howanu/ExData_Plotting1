c547457628a1ce0b224232556d273bfda1b60425
Alex Krowitz <akrowitz@kronos.com>
2014-07-13 16:07:47 -0400

    Added plot1

---
+++ plot1.R
@@ -0,0 +1,26 @@
+# Created for course assignment 1 of Coursera's Exploratory Data Analysis Course
+# URL: https://class.coursera.org/exdata-004/ 
+#
+# The following R script is meant to reproduce plot1.png as it was created by
+# Roger D. Peng of Johns Hopkins. It is displayed by the README.md at this URL:
+# https://github.com/howanu/ExData_Plotting1
+
+##
+## Download, unzip, read, and filter the data
+##
+
+source("readdata.R")
+obs <- readdata()
+
+##
+## Create the plot
+##
+png("plot1.png")
+hist(obs$Global_active_power, 
+     col = "red",
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     ylab = "Frequency"
+     )
+
+dev.off()
